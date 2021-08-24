31fd3bad888f87020918dcda6c4359ce5879ed40
CelsoArmas <ctirado@xternal.biz>
2021-08-24 21:17:07 +0000

    Se agrego el servicio Update table

--- server.js
+++ server.js
@@ -153,6 +153,27 @@ app.post('/api/General/Read_Table/', cors(corsOptionsDelegate),function (req, re
     });    
 });
 
+/**
+ * POST Update table
+ */
+app.post('/api/General/Update_Table/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/General/Update_Table/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
+
 // the server
 const port = process.env.PORT || 3000;  // cloud foundry will set the PORT env after deploy
 app.listen(port, function () {
