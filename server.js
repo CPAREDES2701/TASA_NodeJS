cba78a7096f1730b43008acd06e03d0f8d98eeb4
amagno962021 <amagno@xternal.biz>
2021-08-26 18:39:09 -0500

    master

--- server.js
+++ server.js
@@ -115,14 +115,14 @@ app.get('/api/embarcacion/listaEmbarcacion', cors(corsOptionsDelegate),function 
     });    
 });
 
-//post
-app.post('/api/embarcacion/listaTipoEmbarcacion', cors(corsOptionsDelegate),function (req, res) {  
+//get
+app.get('/api/embarcacion/listaTipoEmbarcacion', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
     _getAccessToken()
     .then((result) => {
         console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
-        var sUrl = HOST + "/api/embarcacion/listaTipoEmbarcacion";
-        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+        var sUrl = HOST + "/api/embarcacion/listaTipoEmbarcacion?usuario=" + req.query.usuario;
+        return _doQUERY(sUrl, result.accessToken, null, 'GET');
     })
     .then((result) => {
         console.log('Successfully called OData service. Response body: ' + result.responseBody);
@@ -133,6 +133,26 @@ app.post('/api/embarcacion/listaTipoEmbarcacion', cors(corsOptionsDelegate),func
         res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
     });    
 });
+
+//get
+app.get('/api/embarcacion/listaPlantas', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/listaPlantas?usuario=" + req.query.usuario;
+        return _doQUERY(sUrl, result.accessToken, null, 'GET');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
+
 
 /**
  * POST Read table
