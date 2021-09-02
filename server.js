5981ed5ff42c81b585a34230ebd460360f9ca1bf
CelsoArmas <ctirado@xternal.biz>
2021-09-02 01:57:59 +0000

    se agrego POST /api/requerimientopesca/registrar

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ app.post('/api/General/AppMaestros/', cors(corsOptionsDelegate),function (req, r
 });
 
 /**
- * POST Requerimiento pesca
+ * POST Requerimiento pesca listar
  */
 app.post('/api/requerimientopesca/listar/', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
@@ -287,6 +287,27 @@ app.post('/api/requerimientopesca/listar/', cors(corsOptionsDelegate),function (
     .then((result) => {
         console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
         var sUrl = HOST + "/api/requerimientopesca/listar/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
+
+/**
+ * POST Requerimiento pesca Registrar
+ */
+app.post('/api/requerimientopesca/registrar', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/requerimientopesca/registrar";
         return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
     })
     .then((result) => {
