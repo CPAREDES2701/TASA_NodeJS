31334db96e96a5af4f18c4784b8b330ca1567ec5
CelsoArmas <ctirado@xternal.biz>
2021-09-09 22:22:11 +0000

    Se agrego busquedas de embarcaciones

--- server.js
+++ server.js
@@ -386,6 +386,27 @@ app.post('/api/embarcacion/MoverEmbarcacion/', cors(corsOptionsDelegate),functio
     });
 });
 
+/**
+ * POST Busqueda embarcacion
+ */
+app.post('/api/embarcacion/BusquedasEmbarcacion/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/BusquedasEmbarcacion/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
 // the server
 const port = process.env.PORT || 3000;  // cloud foundry will set the PORT env after deploy
 app.listen(port, function () {
