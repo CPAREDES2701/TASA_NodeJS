24b9b2f77c5ac67d968cc7e32bf52b766824e8f6
amagno962021 <amagno@xternal.biz>
2021-09-22 08:57:20 -0500

    certificado de bodega

--- server.js
+++ server.js
@@ -239,6 +239,25 @@ app.post('/api/embarcacion/consultaMarea/', cors(corsOptionsDelegate),function (
     });    
 });
 
+//validar ccertificado bodega
+app.post('/api/embarcacion/ValidarBodegaCert/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/ValidarBodegaCert/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
+
 
 /**
  * POST Read table
