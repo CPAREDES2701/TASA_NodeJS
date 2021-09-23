2a7a777fa659f6a74a0ba76696b73c7956a9cd8c
amagno962021 <amagno@xternal.biz>
2021-09-23 04:19:19 -0500

    validar marea

--- server.js
+++ server.js
@@ -258,6 +258,24 @@ app.post('/api/embarcacion/ValidarBodegaCert/', cors(corsOptionsDelegate),functi
     });    
 });
 
+//validar marea
+app.post('/api/embarcacion/ValidarMarea/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/ValidarMarea/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
 
 /**
  * POST Read table
