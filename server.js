542a981e6d7735c77f9499f4a70bccc1f50f1528
Future Technologies <cesargpq@gmail.com>
2021-09-23 09:57:05 -0500

    commit

--- server.js
+++ server.js
@@ -144,6 +144,25 @@ app.post('/api/preciospesca/Mant', cors(corsOptionsDelegate),function (req, res)
 });
 
 
+app.post('/api/preciospesca/ConsultarPrecioMar', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/preciospesca/ConsultarPrecioMar";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
+
+
 
 
 app.post('/api/preciospesca/Leer', cors(corsOptionsDelegate),function (req, res) {  
