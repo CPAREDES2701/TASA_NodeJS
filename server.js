a4edc6dc7a18faf12be836fc35bd79d36b88edd6
pierotoscano <ptoscano@xternal.biz>
2021-10-19 11:22:55 -0500

    Pesca declarada diaria

--- server.js
+++ server.js
@@ -1022,6 +1022,28 @@ app.post('/api/analisiscombustible/Listar', cors(corsOptionsDelegate),function (
     });
 });
 
+/**
+ * Pesca declarada diaria
+ */
+ app.post('/api/sistemainformacionflota/PescaDeclaradaDiara', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/sistemainformacionflota/PescaDeclaradaDiara";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
+
 // the server
 const port = process.env.PORT || 3000;  // cloud foundry will set the PORT env after deploy
 app.listen(port, function () {
