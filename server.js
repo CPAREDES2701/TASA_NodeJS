de445ae9104e1fd8f6458cbd5c89d87ee1d7ed48
CelsoArmas <ctirado@xternal.biz>
2021-10-21 05:43:44 -0500

    Busqueda adicional Embarcaciones

--- server.js
+++ server.js
@@ -779,7 +779,44 @@ app.post('/api/embarcacion/BusquedasEmbarcacion/', cors(corsOptionsDelegate),fun
     });
 });
 
+app.post('/api/embarcacion/ConsultarEmbarcacion/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/ConsultarEmbarcacion/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
 
+/**
+ * POST /api/embarcacion/BusqAdicEmbarca/
+ */
+app.post('/api/embarcacion/BusqAdicEmbarca/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/BusqAdicEmbarca/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
 
 app.post('/api/embarcacion/ConsultarEmbarcacion/', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
