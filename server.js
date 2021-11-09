dd153e9303e4f3d05fa2b293308aec7d5a536f04
amagno962021 <amagno@xternal.biz>
2021-11-09 05:28:11 -0500

    Consulta horometros

--- server.js
+++ server.js
@@ -1660,6 +1660,24 @@ app.post('/api/General/UpdateEmbarcacionMasivo/', cors(corsOptionsDelegate),func
     });
 });
 
+app.post('/api/embarcacion/consultarHorometro/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/consultarHorometro/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
 // the server
 const port = process.env.PORT || 3000;  // cloud foundry will set the PORT env after deploy
 const server = app.listen(port);
