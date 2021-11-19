4351df2a082136ef480dcb9105581941e82ef8e2
Future Technologies <cesargpq@gmail.com>
2021-11-19 15:29:34 -0500

    l

--- server.js
+++ server.js
@@ -984,6 +984,23 @@ app.post('/api/tolvas/ejecutarPrograma', cors(corsOptionsDelegate),function (req
         res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
     });
 });
+app.post('/api/tolvas/validarPeriodo', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/tolvas/validarPeriodo";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
 //ejecutar job
 
 
