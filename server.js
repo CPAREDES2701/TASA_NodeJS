d0a7d51227aebacc84c3c48923bdc8f5f558b8ec
Future Technologies <cesargpq@gmail.com>
2021-11-30 16:58:03 -0500

    f

--- server.js
+++ server.js
@@ -387,6 +387,27 @@ app.post('/api/tripulantes/PDFRolTripulacion/', cors(corsOptionsDelegate),functi
     });    
 });
 
+
+app.post('/api/tripulantes/PDFTrimestral/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/tripulantes/PDFTrimestral/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
+
+
+
 app.post('/api/tripulantes/PDFZarpe/', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
     _getAccessToken()
