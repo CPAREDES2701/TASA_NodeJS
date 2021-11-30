7ff08f79268d0beb2217a9c7938db5477e34ed15
pierotoscano <ptoscano@xternal.biz>
2021-11-30 11:15:41 -0500

    Pesca declarada diaria - diferencias

--- server.js
+++ server.js
@@ -1506,6 +1506,27 @@ app.post('/api/analisiscombustible/Listar', cors(corsOptionsDelegate),function (
 });
 
 /**
+ * Pesca declarada diaria (ver detalles diferencia)
+ */
+ app.post('/api/sistemainformacionflota/PescaDeclaradaDife', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/sistemainformacionflota/PescaDeclaradaDife";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
+/**
  * Pesca descargada
  */
 app.post('/api/sistemainformacionflota/PescaDescargada', cors(corsOptionsDelegate),function (req, res) {  
