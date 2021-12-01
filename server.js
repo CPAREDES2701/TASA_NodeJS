53ec0ab6c5db2b3a649ad74e3c61b3d3bb490858
pierotoscano <ptoscano@xternal.biz>
2021-12-01 10:07:26 -0500

    Reabrir y anular marea

--- server.js
+++ server.js
@@ -1744,6 +1744,53 @@ app.post('/api/sistemainformacionflota/PescaDescargada', cors(corsOptionsDelegat
     });
 });
 
+/**
+ * Gestión de pesca
+ */
+
+/**
+ * Reabrir marea
+ */
+ app.post('/api/gestionpesca/reabrir-marea', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/gestionpesca/reabrir-marea";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
+/**
+ * Anular marea
+ */
+ app.post('/api/gestionpesca/anular-marea', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/gestionpesca/anular-marea";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
+
 // https://cf-nodejs-qas.cfapps.us10.hana.ondemand.com/api/valeviveres/CostoRacionValev
 app.post('/api/valeviveres/CostoRacionValev', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
