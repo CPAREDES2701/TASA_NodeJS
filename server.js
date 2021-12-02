3fa8f9ec9c76878ae21c860c76e06ce6b7b1bbe8
amagno962021 <amagno@xternal.biz>
2021-12-02 16:50:55 -0500

    emba comb

--- server.js
+++ server.js
@@ -2165,6 +2165,24 @@ app.post('/api/embarcacion/ObtenerConfigReservas/', cors(corsOptionsDelegate),fu
     });
 });
 
+app.post('/api/embarcacion/ObtenerEmbaComb/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/embarcacion/ObtenerEmbaComb/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
 app.post('/api/embarcacion/CrearReserva/', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
     _getAccessToken()
