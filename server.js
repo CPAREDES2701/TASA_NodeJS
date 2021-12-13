adb37fad54c343eb83cd0acc195b9f15a35f745b
keltze <cualitativasoft@gmail.com>
2021-12-13 04:37:29 -0500

    Pesca competencia produce

--- server.js
+++ server.js
@@ -1928,6 +1928,27 @@ app.post('/api/sistemainformacionflota/PescaDescargada', cors(corsOptionsDelegat
     });
 });
 
+/**
+ * Pesca de competencia - PRODUCE
+ */
+app.post('/api/sistemainformacionflota/PescaCompetenciaProduce', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/sistemainformacionflota/PescaCompetenciaProduce";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
 // https://cf-nodejs-qas.cfapps.us10.hana.ondemand.com/api/valeviveres/CostoRacionValev
 app.post('/api/valeviveres/CostoRacionValev', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
