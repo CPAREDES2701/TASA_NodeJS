94e1c3629952b4a614d1cf9e0c95520dc2cc1e43
Israel <fernandezpij@gmail.com>
2022-01-19 18:46:01 -0500

    Maestro_ObtenerRegistros

--- server.js
+++ server.js
@@ -954,6 +954,23 @@ app.post('/api/embarcacion/ValidarMarea/', cors(corsOptionsDelegate),function (r
 /**
  * POST Read table
  */
+ app.post('/api/General/Maestro_ObtenerRegistros/', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/General/Maestro_ObtenerRegistros/";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });    
+});
 app.post('/api/General/Read_Table/', cors(corsOptionsDelegate),function (req, res) {  
     console.log('Node server has been invoked. Now calling Backend service API ...');
     _getAccessToken()
